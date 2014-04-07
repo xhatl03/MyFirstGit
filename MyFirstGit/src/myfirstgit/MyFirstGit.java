61a6aa33d26f975fa61439e4bfa3880f5d24633d
xhatl03 <hatle.lukas@gmail.com>
2014-04-08 00:14:34 +0200

    a

--- MyFirstGit/src/myfirstgit/MyFirstGit.java
+++ MyFirstGit/src/myfirstgit/MyFirstGit.java
@@ -16,5 +16,10 @@ public class MyFirstGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("is it working?");
+        System.out.println("not sure yet!");
+<<<<<<< HEAD
+        int s;
+=======
+>>>>>>> 1e190a90dfba159bae3b8f3cd8dae788f1e83bee
     }
 }
